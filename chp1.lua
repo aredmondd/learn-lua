eac2a43e30a791c357d895fca5c736c1fdfa95ca
Aiden Redmond <adnrdmnd@gmail.com>
2025-10-01 07:16:55 -0400

    1.2

--- chp1.lua
+++ chp1.lua
@@ -1,8 +1,23 @@
--- factorial function
+--[[ 
+takeaways
+1. `lua -i -lchp1` allows you to run functions declared in a file in interactive mode
+2. `dofile('chp1.lua')` does a similar thing
+]]
+
+-- 1.1: allow function to take negative values
 function factorial(n)
-    if (n == 0)
+    if n < 0 then
+        return -1
+    end
+    if n == 0 then
         return 1
     else 
         return n * factorial(n-1)
     end
+end
+
+-- 1.2: -l vs dofile
+-- i think i profile the -l version
+function twice(x)
+    return 2.0 * x
 end
